c3af6d5a1e9a58ea8ea1afb91ceacf719de60ab3
Marlon <marlonmisra@gmail.com>
2014-02-18 18:56:04 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Telemed::Application.config.secret_key_base = '9fdee13df228365ee1752cc48feb9dc3eaa65e82ab5c161e5c95c80448153c2e0cdfd88ec0cf63c61daed00b36840b2ce4d45f827fcb830efd486402c00b9b51'
