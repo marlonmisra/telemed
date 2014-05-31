679c228c104ac0552f6a04293ba947d59715cc69
Marlon <marlonmisra@gmail.com>
2014-05-31 12:46:37 -0700

    finishing off user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Telemed::Application.routes.draw do
+  resources :users
+
   root 'static_pages#home'
 
   match '/signup', to: 'users#new', via: 'get'
-  
   match '/about', to: 'static_pages#about', via: 'get'
   match '/login', to: 'static_pages#login', via: 'get'
 
